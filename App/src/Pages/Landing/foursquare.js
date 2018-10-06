3531c06827d047047e791e234190ba90ed7b81cf
Deyuan Zhang <deyuanzhang@Deyuans-MacBook-Pro.local>
2018-10-06 09:46:30 -0500

    filled out cards

--- App/src/Pages/Landing/foursquare.js
+++ App/src/Pages/Landing/foursquare.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { Card, CardText, CardBody,
-  CardTitle, CardSubtitle } from "reactstrap";
+  CardTitle, CardSubtitle, ToolTip } from "reactstrap";
 
 export default class FourSquare extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      tooltipOpen: false
+    };
+  }
+
   render () {
     return (
       <div>
@@ -10,6 +19,27 @@ export default class FourSquare extends React.Component {
           <CardBody>
             <CardTitle>Young Adults</CardTitle>
             <CardSubtitle>Age: 18-24</CardSubtitle>
+            <CardText>Hover over for successful stories!</CardText>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            <CardTitle>Adults</CardTitle>
+            <CardSubtitle>Age: 18 and Higher </CardSubtitle>
+            <CardText>Hover over for successful stories!</CardText>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            <CardTitle>Veteran</CardTitle>
+            <CardSubtitle>Former Member of the Armed Services</CardSubtitle>
+            <CardText>Hover over for successful stories!</CardText>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            <CardTitle>Rehabilitated</CardTitle>
+            <CardSubtitle>A Second Chance</CardSubtitle>
             <CardText>Hover over for successful stories!</CardText>
           </CardBody>
         </Card>
