3faa3411cbc9e51b700525ec5c3fd6cb0301118b
Deyuan Zhang <deyuanzhang@Deyuans-MacBook-Pro.local>
2018-10-06 01:44:01 -0500

    Start checkbox

---
+++ App/src/Pages/Landing/Checkbox.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+
+class Checkbox extends React.Component {
+    constructor(props){
+
+    }
+
+    render() {
+        
+    }
+}
