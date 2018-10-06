300c0efe5b8af9cac9c39d8d0e70e4ce38f01b13
Deyuan Zhang <deyuanzhang@Deyuans-MacBook-Pro.local>
2018-10-06 01:55:56 -0500

    checkbox funcs

--- App/src/Pages/Landing/Checkbox.js
+++ App/src/Pages/Landing/Checkbox.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
 
-class Checkbox extends React.Component {
-    constructor(props){
+export default class Checkbox extends React.Component {
+  constructor (props) {
+    super(props);
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    }
+  render() {
 
-    render() {
-        
-    }
+  }
 }
