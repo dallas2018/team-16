67db8675344a09fefcd28a83e997563e03f0a99a
Deyuan Zhang <deyuanzhang@Deyuans-MacBook-Pro.local>
2018-10-06 03:39:44 -0500

    firebase

--- App/venv/app.py
+++ App/venv/app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 from firebase import firebase 
 
 app = Flask(__name__)
+firebase = firebase.FirebaseApplication('https://code4good-e67ab.firebaseio.com', None)
 
-@app.route('/mainInfo')
+@app.route('/', methods=['GET', 'POST'])
 def mainInfoFunc():
-    return "<h1>Hello, world!</h1>"
+    firebase.post('/films', new_user)
 
 if __name__ == "__main__":
     app.run(debug=True)
