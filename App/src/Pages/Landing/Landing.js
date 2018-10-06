345e493abec6fbcf56e09355bc6d07ea7f1a8cd0
amangotchu <aman.gottu@gmail.com>
2018-10-05 21:12:28 -0500

    Added jumbotron logic

--- App/src/Pages/Landing/Landing.js
+++ App/src/Pages/Landing/Landing.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Jumbotron, Button } from "reactstrap";
 
 class Landing extends React.Component {
   constructor (props) {
@@ -23,7 +24,15 @@ class Landing extends React.Component {
   render () {
     return (
       <React.Fragment>
-        Hello
+        <Jumbotron>
+          <h1 className='display-3'>Hello, world!</h1>
+          <p className='lead'>This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+          <hr classN='my-2' />
+          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+          <p className='lead'>
+            <Button color='primary'>Learn More</Button>
+          </p>
+        </Jumbotron>
       </React.Fragment>
     );
   }
