8ab33c08485ac5d577a4fd466a7ca69f9f23b4b5
amangotchu <aman.gottu@gmail.com>
2018-10-06 00:44:33 -0500

    Added Component 1

--- App/src/Pages/Landing/Landing.js
+++ App/src/Pages/Landing/Landing.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Jumbotron, Button } from "reactstrap";
-
+import TopBar from "./TopBar";
 class Landing extends React.Component {
   constructor (props) {
     super(props);
@@ -24,15 +23,7 @@ class Landing extends React.Component {
   render () {
     return (
       <React.Fragment>
-        <Jumbotron>
-          <h1 className='display-3'>Hello, world!</h1>
-          <p className='lead'>This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
-          <hr classN='my-2' />
-          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-          <p className='lead'>
-            <Button color='primary'>Learn More</Button>
-          </p>
-        </Jumbotron>
+        <TopBar />
       </React.Fragment>
     );
   }
